f855beb212363e7fe75358e8bc13963b1887b60d
DLLM <DLLM@gft.com>
2022-11-17 00:13:37 -0300

    Configurando o hiroku

--- backend/src/main/java/com/dev/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/dev/dsmeta/services/SmsService.java
@@ -37,7 +37,7 @@ public class SmsService {
 			String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 			
 			String msg = "O Vendedor: " + sale.getSellerName() + " foi destaque em "+ date
-					+ " com um total de R% " + String.format("&.2f", sale.getAmount()) + "Supervisionado pelo pika de mel https://www.instagram.com/daniellopes_sm/";
+					+ " com um total de R$ " + String.format("%.2f", sale.getAmount()) + " Supervisionado pelo pika de mel https://www.instagram.com/daniellopes_sm/";
  
 			Twilio.init(twilioSid, twilioKey);
 
